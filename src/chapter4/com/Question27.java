26884290d179e6295a5ecfcbb5f14bbf82bb755b
xiechao <907796939@qq.com>
2018-04-26 21:43:07 +0800

    二叉树遍历延伸

---
+++ src/chapter4/com/Question27.java
@@ -0,0 +1,10 @@
+package chapter4.com;
+
+/**
+ * @author:xiehcao
+ * @Date:2018/4/26
+ * @Time:20:02
+ * @description：
+ */
+public class Question27 {
+}
---
+++ src/chapter4/com/Question28.java
@@ -0,0 +1,10 @@
+package chapter4.com;
+
+/**
+ * @author:xiehcao
+ * @Date:2018/4/26
+ * @Time:20:48
+ * @description：
+ */
+public class Question28 {
+}
